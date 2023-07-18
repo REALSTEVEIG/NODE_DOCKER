62679a278195a9e289cc1af758a397dca7b5e50f
STEPHEN IGNATIUS <stephen.ignatius@gmail.com>
2023-07-18 21:17:48 +0100

    mongo_url 3

--- config/config.js
+++ config/config.js
@@ -1,10 +1,10 @@
 module.exports = {
     MONGO_IP: process.env.MONGO_IP || "mongo",
     MONGO_PORT: process.env.MONGO_PORT || 27017,
-    MONGO_USER: process.env.MONGO_USER,
-    MONGO_PASSWORD: process.env.MONGO_PASSWORD,
+    MONGO_USER: process.env.MONGO_USER || "stephen",
+    MONGO_PASSWORD: process.env.MONGO_PASSWORD || "steve1234",
     REDIS_URL: process.env.REDIS_URL || "redis",  // Use "redis" as the default value
     REDIS_PORT: process.env.REDIS_PORT || 6379,
-    SESSION_SECRET: process.env.SESSION_SECRET
+    SESSION_SECRET: process.env.SESSION_SECRET || "secret",
   };
   
