3c207611e412ec1cbe9359811f20a4ff7e58bfe5
STEPHEN IGNATIUS <stephen.ignatius@gmail.com>
2023-07-19 12:45:40 +0100

    redis_port

--- app.js
+++ app.js
@@ -8,9 +8,14 @@ const redis = require("redis")
 const session = require("express-session")
 const RedisStore = require("connect-redis").default
 
+// let redisClient = redis.createClient({
+//     url: `redis://${REDIS_URL}:${REDIS_PORT}`
+// })
+
 let redisClient = redis.createClient({
-    url: `redis://${REDIS_URL}:${REDIS_PORT}`
-})
+    host: "172.28.0.4",
+    port: REDIS_PORT
+ })
 
 redisClient.connect().then(() => {
     console.log("Connected to Redis...")
@@ -22,7 +27,7 @@ const app = express()
 
 const connectWithRetry = () => {
     // mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`)
-    mongoose.connect(`mongodb://stephen:steve1234@mongo:27017/?authSource=admin`)
+    mongoose.connect(`mongodb://stephen:steve1234@172.28.0.3:27017/?authSource=admin`)
 .then(() => {
     console.log("Connected to mongoDB...")
 })
